f390b0acaf1a41de568fc5ec2e426c2faa0709f5
Hotsuin <131473154+Hotsuin@users.noreply.github.com>
2024-03-22 19:03:04 +0800

    Update Triangle.java

--- Geometric Shapes/Triangle.java
+++ Geometric Shapes/Triangle.java
@@ -8,6 +8,7 @@ public class Triangle extends Shape{
     private double side3;
     
     public Triangle (String name, double s1, double s2, double s3){
+        super(name);
         side1 = s1;
         side2 = s2;
         side3 = s3;
@@ -15,8 +16,8 @@ public class Triangle extends Shape{
 
 
     public double area(){
-            //will complete later lmao
-        return 
+        double s = (side1 + side2 + side3)/2;
+        return Math.sqrt(s * (s - side1) * (s - side2) * (s - side3));
     }
         
       
