1a4559cedc0a82752d3b060f85e68e0bb73bfbf8
Hotsuin <131473154+Hotsuin@users.noreply.github.com>
2024-03-22 14:36:10 +0800

    Update Triangle.java

--- Geometric Shapes/Triangle.java
+++ Geometric Shapes/Triangle.java
@@ -1,1 +1,28 @@
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
 
+public class Triangle extends Shape{
+    //instantiates the 
+    private double side1;
+    private double side2;
+    private double side3;
+    
+    public Triangle (String name, double s1, double s2, double s3){
+        side1 = s1;
+        side2 = s2;
+        side3 = s3;
+    }
+
+
+    public double area(){
+            //will complete later lmao
+        return 
+    }
+        
+      
+    
+    public double perimeter(){
+        return side1 + side2 + side3;
+    }
+    
+}//end of class
